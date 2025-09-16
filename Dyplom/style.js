804e4f7ad2a851edc431db634b8862a3d788ba25
Stepan <chedoorek@gmail.com>
2025-09-16 23:02:13 +0200

    Add new File and new Folder

---
+++ Dyplom/style.js
@@ -0,0 +1,28 @@
+const ImgFish = document.getElementById("ImgFish");
+
+function RotateFish() {
+
+    ImgFish.addEventListener("mouseenter", () => {
+        ImgFish.style.transition = "transform 2s ease";
+        ImgFish.style.transform = "rotate(360deg)";
+    });
+
+    ImgFish.addEventListener("mouseleave", () => {
+        ImgFish.style.transition = "transform 1s ease";
+        ImgFish.style.transform = "rotate(0deg)";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", RotateFish);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+      effect: "fade",
+    autoplay: {
+      delay: 5000, 
+    },
+  });
+});
+
+  /* Новая ветка */
